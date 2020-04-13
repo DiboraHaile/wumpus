e9cd641a6ef9c2da80e9b12babff89bf2d606fa5
DiboraHaile <deborahaile3@gmail.com>
2020-04-13 22:15:24 +0300

    add method to update history

--- agent.cpp
+++ agent.cpp
@@ -5,100 +5,91 @@
 #include <map>
 //#include "tablox.hpp"
 //#include "evaluate.hpp"
-//#include "rules.hpp"
-//#include "environment.hpp"
+#include "rules.hpp"
+#include "environment.hpp"
+#include "agent.hpp"
 using namespace std;
 
 
-class Agent{
+Agent::Agent(){
+    // start agent from pos 0,0
+    current_pos.x = 0;
+    current_pos.y = 0;
+    // set all room to unvisited
+    for (int i=0;i<4;i++)
+        for (int j=0;j<4;j++)
+            agent_history.V[i][j] = false;
+    okay_rooms.push(current_pos);
     
-    public:
-    map<string,string> rule_match;
-    struct history{
-        bool S[4][4];
-        bool B[4][4];
-        bool V[4][4];
-        bool Ok[4][4];
-        bool W[4][4];
-        bool G[4][4];
-    };
 
-    struct pos{
-        int x;
-        int y;
-    };
+}
 
-    history hist_agent;
-    stack<pos> okay_rooms;
-    
-    
-    Agent(){
-        // start agent from pos 0,0
-        pos current_pos;
-        current_pos.x = 0;
-        current_pos.y = 0;
-        // set all room to unvisited
-        for (int i=0;i<4;i++)
-            for (int j=0;j<4;j++)
-                hist_agent.V[i][j] = false;
-        
+position Agent::Move (position destination_pos){
+    return destination_pos;
 
-    }
-    void Senses(pos current_pos){
-
-        //return sense;
-    }
-    pos Move (pos destination_pos){
-        pos position;
-        //position.x = current_pos.x;
-        //position.y = current_pos.y;
-        /*if (dir == "forward" and current_pos.y<3)
-            position.y = current_pos.y + 1;
-        else if(dir == "backward" and current_pos.y>0)
-            position.y = current_pos.y - 1;
-        else if(dir == "left" and current_pos.x>0)
-            position.x = current_pos.x - 1;
-        else if(dir == "right" and current_pos.x<3)
-            position.x = current_pos.x + 1;
-        else
+}
+// gives us position of 
+void Agent::Actuates(position ele_pos,string element){
+    if (element == "wumpus")
         {
+        // set every other room wumpus variable false
+        // since we only have one wumpus
+        for(int i=0;i<4;i++)
+            for(int j=0;j<4;j++)
+                agent_history.W[i][j] = false;
+        agent_history.W[ele_pos.x][ele_pos.y] = true;
+        }
+    else if (element == "gold"){
+        Move(ele_pos);
+        agent_history.G[ele_pos.x][ele_pos.y] = true;
+        cout<<"game over";
+    }
+    else if (element == "okay"){
+        okay_rooms.push(ele_pos);
+        agent_history.Ok[ele_pos.x][ele_pos.y];
+    }
+    else{
+
+    }
     
-        }*/
-        return destination_pos;
-
-    }
-    // gives us position of 
-    void Actuates(pos position,string element){
-        if (element == "wumpus")
-            {
-            // set every other room wumpus variable false
-            // since we only have one wumpus
-            for(int i=0;i<4;i++)
-                for(int j=0;j<4;j++)
-                    hist_agent.W[i][j] = false;
-            hist_agent.W[position.x][position.y] = true;
-            }
-        else if (element == "gold"){
-            Move(position);
-            hist_agent.G[position.x][position.y] = true;
-            cout<<"game over";
-        }
-        else if (element == "okay"){
-            okay_rooms.push(position);
-            hist_agent.Ok[position.x][position.y];
-        }
-        else{
-
-        }
         
-            
-    }
+}
 
-    void initialize_movt(){
+void Agent::update_history(perception env_percept,position current_pos){
+    agent_history.B[current_pos.x][current_pos.y] = false;
+    agent_history.G[current_pos.x][current_pos.y] = false;
+    agent_history.S[current_pos.x][current_pos.y] = false;
+    agent_history.P[current_pos.x][current_pos.y] = false;
+    agent_history.W[current_pos.x][current_pos.y] = false;
+
+    for (int i=0;i<env_percept.B.size();i++)
+        if (current_pos.x == env_percept.B[i].x and current_pos.y == env_percept.B[i].y)
+            agent_history.B[current_pos.x][current_pos.y] = true;
+    for (int i=0;i<env_percept.S.size();i++)
+        if (current_pos.x == env_percept.S[i].x and current_pos.y == env_percept.S[i].y)
+            agent_history.S[current_pos.x][current_pos.y] = true;
+    for (int i=0;i<3;i++)
+        if (current_pos.x == env_percept.P[i].x and current_pos.y == env_percept.P[i].y)
+            agent_history.P[current_pos.x][current_pos.y] = true;
+    
+    if (current_pos.x == env_percept.G[0].x and current_pos.y == env_percept.G[0].y)
+            agent_history.G[current_pos.x][current_pos.y] = true;
+    if (current_pos.x == env_percept.W[0].x and current_pos.y == env_percept.W[0].y)
+            agent_history.W[current_pos.x][current_pos.y] = true;
+
+    agent_history.V[current_pos.x][current_pos.y] = true;
+}
+
+void Agent::Decide(history a_hist, position cur_pos){
+    stack<position> neighs_cur;
+    position neigh;
+    neighs_cur = neighbours(cur_pos);
+    while(!neighs_cur.empty()){
+        neigh = neighs_cur.top();
+        neighs_cur.pop();
+        if (!no_pit(neigh) and !no_wumpus(neigh))
+            okay_rooms.push(cur_pos);
         
     }
-    void Decide(history){
-        
-    }
+}
 
-};
