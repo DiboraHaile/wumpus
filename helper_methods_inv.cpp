38ef12eca3017df375a74acda976c0b714f8cf44
DiboraHaile <deborahaile3@gmail.com>
2020-04-13 22:23:09 +0300

    add inverse helper methods

---
+++ helper_methods_inv.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include "helper_methods_inv.hpp"
+
+struct operand_vals{
+        bool truth_vs[2];
+        bool branch;
+    };
+
+operand_vals helper_methods_inv::INot(bool result){
+    int operand_no = 1;
+    operand_vals not_op;
+    not_op.truth_vs[0] = !result;
+    return not_op;
+}
+operand_vals helper_methods_inv::IAnd(bool result){
+    int operand_no = 2;
+    operand_vals op;
+   if (result)
+    {
+        op.truth_vs[0] = 1;
+        op.truth_vs[1] = 1;
+        op.branch = 0;
+        return op;
+    }    
+    else
+    {
+        op.truth_vs[0] = 0;
+        op.truth_vs[1] = 0;
+        op.branch = 1;
+        return op;
+    }
+}
+
+operand_vals helper_methods_inv::IOr(bool result){
+   int operand_no = 2;
+    operand_vals op;
+    if (result)
+    {
+        op.truth_vs[0] = 1;
+        op.truth_vs[1] = 1;
+        op.branch = 1;
+        return op;
+    }    
+    else
+    {
+        op.truth_vs[0] = 0;
+        op.truth_vs[1] = 0;
+        op.branch = 0;
+        return op;
+    }
+    }
+
+
+operand_vals helper_methods_inv::IImp(bool result){
+     int operand_no = 2;
+    operand_vals op;
+    if (result)
+    {
+        op.truth_vs[0] = 0;
+        op.truth_vs[1] = 1;
+        op.branch = 1;
+        return op;
+    }    
+    else
+    {
+        op.truth_vs[0] = 1;
+        op.truth_vs[1] = 0;
+        op.branch = 0;
+        return op;
+    }
+}
+
+operand_vals helper_methods_inv::IBimp(bool result){
+
+}
