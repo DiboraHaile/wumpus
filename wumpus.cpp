a36d8a753b431c59ae26423b089de4858aa21fb2
DiboraHaile <deborahaile3@gmail.com>
2020-04-13 22:10:02 +0300

    create interface

---
+++ wumpus.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "environment.hpp"
+#include "agent.hpp"
+using namespace std;
+
+int main(){
+    environment wumpus_env = environment();
+    
+    //wumpus_env.Senses()
+
+}
