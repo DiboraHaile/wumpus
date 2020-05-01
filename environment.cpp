24cc70543e67e36cb3af0f603084a5f4f1ef0e1d
DiboraHaile <deborahaile3@gmail.com>
2020-05-02 01:49:33 +0300

    separate create environment and display environment functions

--- environment.cpp
+++ environment.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h> 
 #include <vector>
 #include "environment.hpp"
+
 //#include "rules.hpp"
 #include "agent.hpp"
 using namespace std;
@@ -14,17 +15,25 @@ environment:: environment(){
     position ok_room_pos;
     // creating an agent
     Agent my_agent = Agent();
-    env_percept = display_envt(my_agent.current_pos);
-    //while(!my_agent.okay_rooms.empty()){
+    env_percept = create_envt(my_agent.current_pos);
+    int ji = 0;
+    //ji<2
+    //
+   while(!my_agent.okay_rooms.empty()){
+        
         ok_room_pos = my_agent.okay_rooms.top();
         my_agent.current_pos = my_agent.Move(ok_room_pos);
-        my_agent.update_history(env_percept,my_agent.current_pos);
-        //cout<<my_agent.agent_history.B[my_agent.current_pos.x][my_agent.current_pos.y]<<"\n";
-        my_agent.Decide(my_agent.agent_history,my_agent.current_pos);
         my_agent.okay_rooms.pop();
-        delay(1);
+        my_agent.update_history(env_percept,my_agent.current_pos);
+        my_agent.Decide(my_agent.agent_history,my_agent.current_pos);
+        //cout<<"\n"<<my_agent.okay_rooms.top().x<<my_agent.okay_rooms.top().y<<"\n";
+        system("clear");
+        //my_agent.visited_rooms.push()
+        display_envt(env_percept,my_agent.current_pos);
+        delay(2000);
+        //cout<<"\n"<<my_agent.current_pos.x<<my_agent.current_pos.y<<flush;
         
-    //}
+   }
     // if out of this loop means okay rooms is empty
     // so we call decide with the element empty
     my_agent.Actuates(my_agent.current_pos,"empty");
@@ -85,7 +94,8 @@ stack<position> environment::neighbours(position pos){
     return neighs;
 
 }
-perception environment::display_envt(position agent_pos){
+
+perception environment::create_envt(position agent_pos){
     pos_arr pos;  
     stack<position> neigh_stack;
     int t = 0;
@@ -96,15 +106,8 @@ perception environment::display_envt(position agent_pos){
     pos = generate_positions();
 
 
-for (int i=0;i<4;i++){
-    for (int j=0;j<4;j++){
-        grid[i][j] = "0";
-    }
-}
-
-
 for (int t=0;t<5;t++){
-    grid [pos.arr[t].x][pos.arr[t].y] = ele[t];
+    //grid [pos.arr[t].x][pos.arr[t].y] = ele[t];
     neigh_stack = neighbours(pos.arr[t]);
     // set environment elements W,P,G
     if (ele[t] == 'W')
@@ -117,36 +120,37 @@ for (int t=0;t<5;t++){
     while(!neigh_stack.empty()){
         popped_neighbour = neigh_stack.top();
         neigh_stack.pop();
-        if (ele[t] == 'P'){       
+        if (ele[t] == 'P')  
             env_perception.B.push_back(popped_neighbour);
-            if ((grid[popped_neighbour.x][popped_neighbour.y]) == "0")
-                grid[popped_neighbour.x][popped_neighbour.y] = "b";
-            else if ((grid[popped_neighbour.x][popped_neighbour.y]) != "b")
-                grid[popped_neighbour.x][popped_neighbour.y] += "b";           
-        }        
-
-            
-        else if (ele[t] == 'W'){
-            env_perception.W.push_back(pos.arr[t]);
-            if ((grid[popped_neighbour.x][popped_neighbour.y]) == "0")
-                grid[popped_neighbour.x][popped_neighbour.y] = "s";
-            else if ((grid[popped_neighbour.x][popped_neighbour.y]) != "s")
-                grid[popped_neighbour.x][popped_neighbour.y] += "s";
-        }
-  
-        else
-        {
-            
-        }
-        neighbours(pos.arr[t]).pop();
+        else if (ele[t] == 'W')
+            env_perception.S.push_back(popped_neighbour);
     }
             
 }
-    // print agent
-    if (grid[agent_pos.x][agent_pos.y] != "0")
-        grid[agent_pos.x][agent_pos.y] += "X";
-    else
-        grid[agent_pos.x][agent_pos.y] = "X";
+ return env_perception;
+}
+
+void display_envt(perception env, position cur_p){
+    string grid[4][4];
+    for (int i=0;i<4;i++){
+    for (int j=0;j<4;j++){
+        grid[i][j] = "0";
+               
+    }
+    }
+    for (int t=0;t<env.B.size();t++)
+        grid[env.B[t].x][env.B[t].y] = "b";
+    for (int k=0;k<env.S.size();k++)
+        if (grid[env.S[k].x][env.S[k].y] == "b")
+            grid[env.S[k].x][env.S[k].y] = "bs"; 
+        else
+            grid[env.S[k].x][env.S[k].y] = "s";
+    for (int r=0;r<env.P.size();r++)
+         grid[env.P[r].x][env.P[r].y] = "P";
+    grid[env.G[0].x][env.G[0].y] = "G";
+    grid[env.W[0].x][env.W[0].y] = "W";
+
+    grid[cur_p.x][cur_p.y] = "X";
     
    for (int i=0;i<4;i++){
     for (int j=0;j<4;j++){
@@ -154,8 +158,10 @@ for (int t=0;t<5;t++){
     }
     cout<<"\n\n\n\n";
     }
-}
+    cout<<"\n\n\n";
 
+
+}
 void environment::print_agent(position current_pos){
     grid[current_pos.x][current_pos.y] = "x";
     cout<<grid[current_pos.x][current_pos.y];
